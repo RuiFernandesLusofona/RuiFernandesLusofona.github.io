06543339582234297a0b69eb7cba8d11e81e272b
RuiFernandesLusofona <a22407079@alunos.ulht.pt>
2024-11-20 17:16:53 +0000

    .

--- lab9/main.js
+++ lab9/main.js
@@ -1,11 +1,21 @@
-import {produtos} from './produtos.js';
+import { produtos } from './produtos.js';
 
 const listaProdutos = document.querySelector("produtos")
 
-document.addEventListener('DOMContentLoaded', () => {
+function criarProduto(produto) {
+    
+}
+
+function carregarProdutos(produtos) {
     produtos.forEach(produto => {
 
         criarProduto(produto)
-        listaProdutos.append(criarArticle(produto))
+        listaProdutos.append(criarProduto(produto))
     })
-})  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    carregarProdutos(produtos)
+})
+
